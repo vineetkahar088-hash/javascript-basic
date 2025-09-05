2b7d2977e8d1659c38fe3d98fed0286c651764ed
vineetkahar088-hash <vineetkahar088@gmail.com>
2025-09-05 09:46:58 -0700

    Add files via upload

---
+++ class1.js/index.js
@@ -0,0 +1,50 @@
+    // console.log("Start");
+// let h1Tag=document.createElement("h1")
+// h1Tag.innerText="this is heading h1"
+// console.log(h1Tag)
+// document.body.append(h1Tag)
+
+
+// const divEle = document.getElementById("div-one");
+// let p = document.createElement("p");
+// let span = document.createElement("span");
+// span.innerText = "This is a span tag"
+// p.textContent = "This is a paragraph"
+// console.log(p)
+
+//appendChild is used to append single element at atime
+// divEle.appendChild(p);
+
+//append is used to append multiple element at atime
+// divEle.append(p,span)
+
+// divEle.after(p)
+
+// divEle.before(p)
+
+// const divSecond = document.getElementsByClassName("div-second");
+// console.log(divSecond);
+// let h1Ele = divSecond[0].children[0];
+// let h4 = document.createElement("h4");
+// h4.innerText = "This is a heading 4";
+//this add on 1st place in any parent inside
+// divSecond[0].prepend(h4);
+//this add on last place any parent inside
+// divSecond[0].appendChild(h4)
+
+
+//append in desire position
+// divSecond[0].appendChild(h4)
+// divSecond[0].children[0].appendChild(h4);
+// <h4>​This is a heading 4​</h4>
+// divSecond[0].children[1].after(h4);
+// divSecond[0].children[1].before(h4);
+// divSecond[0].children[0].removeChild(h1)
+
+//this is for removing the element
+// divSecond[0].removeChild(h1Ele)
+
+
+//update the content of any desire element
+
+// h1Ele.innerText = "this is"
