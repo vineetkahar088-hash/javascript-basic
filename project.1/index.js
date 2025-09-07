97ed22a903944f5cc84db7230f8ee075a7a5f931
vineetkahar088-hash <vineetkahar088@gmail.com>
2025-09-07 09:00:56 -0700

    Add files via upload

---
+++ project.1/index.js
@@ -0,0 +1,17 @@
+// console.log("***********************")
+// let divEle = document.getElementsByClassName("div1");
+// console.log(divEle)
+
+//ye work nhi karega every btn click me kuoki ek bar hee paragraph banaya
+// let pEle = document.createElement("p");
+// pEle.textContent = "this is a paragraph"
+// console.log(pEle)//<p>this is a paragraph</p>
+
+// const btnEle = document.getElementById("btnId");
+
+// btnEle.addEventListener('click', () => {
+//     let pEle = document.createElement("p");
+//     pEle.textContent = "this is a paragraph via js"
+//     divEle[0].prepend(pEle);
+//     console.log("This is clicked")
+// })
