44528396c0504a277b608b787a8b805450ac336a
vineet <vineetkahar088@gmail.com>
2025-09-11 08:10:51 -0700

    Add files via upload

---
+++ project.js/index.js
@@ -0,0 +1,20 @@
+let divele=document.getElementsByClassName("div1");
+
+let updatebtn =document.getElementById("update");
+
+let deletebtn =document.getElementById("delete");
+
+updatebtn.addEventListener("click",()=>{
+
+    let childdiv = document.createElement("div");
+
+    let pele = document.createElement("p");
+
+    pele.textContent
+
+    pele.textContent=("update button");
+
+    let updele =document.createElement("button");
+
+    updele.textContent=("delete button");
+})
