798c986abdd6b36c704e894935fc73eb719c5f87
vineet <vineetkahar088@gmail.com>
2025-09-17 08:55:06 -0700

    Add files via upload

---
+++ DSA.js/index.js
@@ -0,0 +1,49 @@
+// it is a concatination
+// let a = 10;
+// let b = "20";
+// console.log(typeof(a+b));
+
+// let a = 10;
+// let b = 20;
+// console.log("sum of 10 and 20" + a + b); ans = 1020
+// console.log("sum of 10 and 20" + (a+b)); ans = 30
+// console.log("1"-1); ans = 0 type corestion (- is a subtract karna)
+
+
+// type swaping
+// let a = 10;
+// let b = 20;
+// let c;
+
+// c = a; 
+// a = b; 
+// b = c; 
+// console.log(a);
+// console.log(b);
+
+// let a = 10;
+// let b = 20;
+
+// a = a + b;
+// b = a - b;
+// a = a - b;
+// console.log(a , b);
+
+// let a = 7;
+// let b = 2;
+
+// console.log(b%a);  ans = 2;
+// console.log(a%b); ans = 3;
+
+// >, <, <=, >=, !==, =, ==, ===
+// console.log(10>5) ans = true;
+// console.log(10<5) ans = false;
+// console.log(10<=15); ans = true;
+// console.log(10 !== 10); ans = false; left or right barabr nahi hone chaiye
+//  13 == 12 dono side chek karta h ki value barabr h ki nahi h agar barabar h to true or barabar nahi h to false
+
+// logical operator
+// && ||
+// multipul condition chek karne ke liye ye use karte h
+// console.log(10>6 && 5<9); ans = true , true 
+// console.log(10>6 && 10<9) ans = false kyunki jo logical ooerator && laga h agar dono side ki condition true h to hii wo true deta h other wise it will be false 
